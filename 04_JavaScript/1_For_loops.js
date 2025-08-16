6df325452a37416a02d6b009367b7d8b1e269bc6
Abuturab <mdimam0703@gmail.com>
2025-08-16 12:21:58 +0530

    added notes

---
+++ 04_JavaScript/1_For_loops.js
@@ -0,0 +1,11 @@
+// Loops
+// Used to iterate a piece of code
+
+// for loop:-
+// for(initialisation; condition; updation) {
+//      //do something
+// }
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
