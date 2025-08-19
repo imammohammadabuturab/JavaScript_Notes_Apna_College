0071b552bf7217536f289a75565d1a84f52b3eee
Imam Mohammad Abuturab <mdimam0703@gmail.com>
2025-08-19 06:22:52 +0530

    added notes

--- 04_JavaScript/5_Print_Multiplication_Table.js
+++ 04_JavaScript/5_Print_Multiplication_Table.js
@@ -1,5 +1,11 @@
-let table = 5
+let n = prompt("Write your number: ");
+n = parseInt(n);
 
-for (let i = 1; i <= 50; i=i+5) {
-    console.log(table)
+for (let i = n; i <= n*10; i=i+n) {
+    console.log(i);
 }
+
+
+// for (let i = 5; i <= 50; i=i+5) {
+//     console.log(i);
+// }
---
+++ 04_JavaScript/8_Favorite_Movie_Guess.js
@@ -0,0 +1,13 @@
+const favMovie = "avatar";
+
+let guess = prompt("guess my favorite movie"); // write "quit" to quit the game
+
+while ((guess != favMovie) && (guess != "quit")) {
+    guess = prompt("wrong guess. please try again");
+}
+
+if(guess == favMovie) {
+    console.log("congrats!!");
+} else {
+    console.log("you quit")
+}
