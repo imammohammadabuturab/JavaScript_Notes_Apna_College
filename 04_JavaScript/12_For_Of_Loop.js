647b1d2a7624677ff1e48079efc220769003e88a
Abuturab <mdimam0703@gmail.com>
2025-08-20 19:22:16 +0530

    added notes

---
+++ 04_JavaScript/12_For_Of_Loop.js
@@ -0,0 +1,13 @@
+// for (element of collection) {
+// //do something
+// }
+
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+
+for(fruit of fruits) {
+    console.log(fruit);
+}
+
+for(charecter of "apnacollege") {
+    console.log(char);
+}
--- 04_JavaScript/8_Favorite_Movie_Guess.js
+++ 04_JavaScript/8_Favorite_Movie_Guess.js
@@ -2,12 +2,27 @@ const favMovie = "avatar";
 
 let guess = prompt("guess my favorite movie"); // write "quit" to quit the game
 
-while ((guess != favMovie) && (guess != "quit")) {
-    guess = prompt("wrong guess. please try again");
+// while ((guess != favMovie) && (guess != "quit")) {
+//     guess = prompt("wrong guess. please try again");
+// }
+
+// if(guess == favMovie) {
+//     console.log("congrats!!");
+// } else {
+//     console.log("you quit")
+// }
+
+
+// Using break keyword
+
+while (guess != favMovie) {
+    if (guess == "quit") {
+        console.log("you quit");
+        break;
+    }
+    guess = prompt("Wrong guess, please try again");
 }
 
-if(guess == favMovie) {
-    console.log("congrats!!");
-} else {
-    console.log("you quit")
+if (guess == favMovie) {
+    console.log("Congrats!!");
 }
